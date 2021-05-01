1dabe9a3206aa0892461fcb490669329c9c5484b
Matías Barrios <matias.barriosn@gmail.com>
2021-05-01 11:07:15 -0400

    feat: hardcode filter

--- apps/luckyseven-finance-frontend/src/app/components/Carta.tsx
+++ apps/luckyseven-finance-frontend/src/app/components/Carta.tsx
@@ -1,19 +1,35 @@
-import Character from '../utils/Character'
+import Character from '../utils/Character';
 
 function Carta() {
+  const name = 'summer';
   const Personaje = Character('character');
   return (
     <div>
-      {Personaje?.results.map((psj) => (
-        <div className="card" style={{ width: '18rem', display: "inline-block"}}>
-          <img src={psj.image} className="card-img-top" style={{display:'block'}}></img>
-          <div className="card-body">
-            <h1 key={psj.id} style={{fontSize:'2rem',textAlign:'center'}}>{psj.name}</h1>
-            <p className="card-text">{psj.species}</p>
-          </div>
-        </div>
-      ))}
-     </div>
+      {Personaje?.results.map(
+        (psj) =>
+          new RegExp(name, 'i').test(psj.name) && (
+            <div
+              className="card"
+              style={{ width: '18rem', display: 'inline-block' }}
+            >
+              <img
+                src={psj.image}
+                className="card-img-top"
+                style={{ display: 'block' }}
+              ></img>
+              <div className="card-body">
+                <h1
+                  key={psj.id}
+                  style={{ fontSize: '2rem', textAlign: 'center' }}
+                >
+                  {psj.name}
+                </h1>
+                <p className="card-text">{psj.species}</p>
+              </div>
+            </div>
+          )
+      )}
+    </div>
   );
 }
 export default Carta;
--- apps/luckyseven-finance-frontend/src/app/components/navbar.tsx
+++ apps/luckyseven-finance-frontend/src/app/components/navbar.tsx
@@ -1,18 +1,27 @@
-
+import { useState } from 'react';
 
 function Navbar() {
+  const [filter, setFilter] = useState('');
+
   return (
     <div>
-      <nav className="navbar navbar-light bg-success" style={{height:'5rem'}}>
+      <nav
+        className="navbar navbar-light bg-success"
+        style={{ height: '5rem' }}
+      >
         <div className="container-fluid">
           <a className="navbar-brand">Rick and Morty browser</a>
           <form className="d-flex">
             <input
+              onChange={(event) => {
+                console.log(event);
+                setFilter(event.target.value);
+              }}
               className="form-control me-2"
               type="search"
               placeholder="Search"
               aria-label="Search"
-            ></input>
+            />
             <button className="btn btn-dark" type="submit">
               Search
             </button>
