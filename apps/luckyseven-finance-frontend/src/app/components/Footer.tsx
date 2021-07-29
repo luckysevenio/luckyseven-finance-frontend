e565f6c654ad4dcecf9c5671f8fa6a8a6308206b
Jorge <jorge.barriosn@usm.cl>
2021-07-29 17:09:12 -0400

    fix: footer info deleted

--- apps/luckyseven-finance-frontend/src/app/components/Footer.tsx
+++ apps/luckyseven-finance-frontend/src/app/components/Footer.tsx
@@ -16,35 +16,20 @@ const StyledApp = styled.div`
 `;
 
 function Footer() {
-          return (
-            <StyledApp>
-            <div className="footer">
-              <div className="container p-4">
-                <div className="row">
-                  <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-                    <h5 className="text-uppercase">Contacto</h5>
-                    <p>Contacto1: +56225678912</p>
-                    <p>Contacto2: +56225678913</p>
-                  </div>
-                  <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-                    <h5 className="text-uppercase">Direccion</h5>
-                    <p>
-                    Avenida Providencia  ....
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="text-center p-3"
-              >
-                © 2021 Copyright:
-                <a className="text-light" href="#">
-                  luckyseven
-                </a>
-              </div>
-            </div>
-            </StyledApp>
-          );
-        }
+  return (
+    <StyledApp>
+      <div className="footer">
+        <div
+          className="text-center p-3"
+        >
+          © 2021 Copyright:
+          <a className="text-light" href="#">
+            luckyseven
+          </a>
+        </div>
+      </div>
+    </StyledApp>
+  );
+}
 
 export default Footer;
