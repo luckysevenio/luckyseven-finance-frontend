c0fee95710f1d7cbd4fef0451e43bad61db2da08
Jorge <jorge.barriosn@usm.cl>
2021-08-12 17:53:06 -0400

    feat: transactions model

--- apps/luckyseven-finance-frontend/src/app/app.tsx
+++ apps/luckyseven-finance-frontend/src/app/app.tsx
@@ -98,6 +98,7 @@ export function App() {
                 <Payments/>
               </Route>
               <Route path="/transacciones">
+                <hr/>
                <Transactions/>
               </Route>
               <Route path="/">
